5b5649c9c16e163db59a2db37817ea11d7f70174
zackees <z@zackees.com>
2025-02-10 22:56:34 -0800

    chore: update dependencies and version information

--- ytclip/ytclip.py
+++ ytclip/ytclip.py
@@ -1,6 +1,6 @@
 """
-    Downloads and clips videos from youtube, rumble, bitchute (using yt-dlp) and clips
-    the video using ffmpeg.
+Downloads and clips videos from youtube, rumble, bitchute (using yt-dlp) and clips
+the video using ffmpeg.
 """
 
 import os
